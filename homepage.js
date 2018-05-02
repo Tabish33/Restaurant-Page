1ab2d472a608af69f8ef4264c7fb9d65ec60c34b
Tabish Bashir <tabishbashir@gmail.com>
2018-05-02 13:07:36 +0530

    Add contact module and tab switching functionality

--- homepage.js
+++ homepage.js
@@ -8,9 +8,9 @@ const homepageLoader = () => {
 
       let nav = document.createElement("nav");
       nav.innerHTML = `<ul>
-                          <li>Home</li>
-                          <li>Menu</li>
-                          <li>Contact</li>
+                          <li class="homeTab">Home</li>
+                          <li class="menuTab">Menu</li>
+                          <li class="contactTab">Contact</li>
                         </ul>`
       parent.appendChild(nav);
 
@@ -27,5 +27,18 @@ const homepageLoader = () => {
 
 }
 
+function homeContent(){
 
-export {homepageLoader};
+      let description = document.querySelector(".description");
+      
+      description.innerHTML = `<p>Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+          in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+          qui officia deserunt mollit anim id est laborum.</p>`
+}
+
+
+export {homepageLoader, homeContent};
