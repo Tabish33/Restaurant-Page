97a2c37f68b5ca383c29351c3bf9d3c33fbb4c8f
Tabish Bashir <tabishbashir@gmail.com>
2018-05-02 13:07:36 +0530

    Add contact module and tab switching functionality

---
+++ dist/contact.js
@@ -0,0 +1,7 @@
+function contactContent(){
+
+    let description = document.querySelector(".description");
+    description.innerHTML = "+41-1214-4313-12";
+}
+
+export {contactContent};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-import {homepageLoader} from '../dist/homepage.js';
+import {homepageLoader , homeContent} from '../dist/homepage.js';
 import {menuContent} from '../dist/menu.js';
+import {contactContent} from '../dist/contact.js';
 
 homepageLoader();
-menuContent();
+
+(function switchTab(){
+    let menuTab = document.querySelector(".menuTab");
+    menuTab.onclick = function(){
+        menuContent();
+    }
+
+    let homeTab = document.querySelector(".homeTab");
+    homeTab.onclick = function(){
+        homeContent();
+    }
+
+    let contactTab = document.querySelector(".contactTab");
+    contactTab.onclick = function(){
+        contactContent();
+    }
+
+})();
